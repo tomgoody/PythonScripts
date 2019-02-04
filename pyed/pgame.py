8caf4b0edfdc885c30e7d55debdb39f9039a9e54
Thomas Goody <tgoody@trustwave.com>
2019-02-03 20:18:25 -0700

    button works on click until movement

--- pyed/pgame.py
+++ pyed/pgame.py
@@ -1,7 +1,7 @@
 from sys import exit
 import pygame
 from pygame.locals import *
-import time
+#import time
 import random
 
 pygame.init()
@@ -11,18 +11,38 @@ SCREEN_SIZE = (800, 600)
 
 #Initializing screen with no caption
 pgDisplay = pygame.display.set_mode(SCREEN_SIZE, pygame.NOFRAME)
+pgDisplay1 = pygame.display.set_mode(SCREEN_SIZE, pygame.NOFRAME)
+#clock = pygame.time.Clock()
+buttons = []
 
-clock = pygame.time.Clock()
-button = []
+myfont = pygame.font.SysFont('Comic Sans MS', 50)
+title = myfont.render('P-Pie!', True, (0, 0, 0))
 
-myfont = pygame.font.SysFont('Comic Sans MS', 30)
-title = myfont.render('P-Pie!', True, (255,255,255))
+#Colors
+green = (0, 125, 0)
 
 class Window():
     def __init__(self, screen, position, size):
         self.screen = screen
         self.rect = Rect(position, size)
 
+
+class Button():
+    def __init__(self, parent, position, size, color):
+        self.parent = parent
+        self.position = position
+        self.size = size
+        self.color = color
+
+    def get_event(selfs, event):
+        pgDisplay.fill(125, 125, 125)
+        pygame.display.flip()
+
+    def create_button(self):
+        newwindow = Window(pgDisplay1, (10, 10), SCREEN_SIZE)
+        newbutton = Button(newwindow, (150, 250), (100, 50), pygame.Color('Pink'))
+        buttons.append(newbutton)
+        newbutton.draw()
 
 
 
@@ -36,13 +56,21 @@ def game_intro():
                 pygame.quit()
                 quit()
 
-        pgDisplay.fill((0,0,0))
+        pgDisplay.fill((255, 255, 255))
+        pgDisplay.blit(title, (350, 10))
 
+        pygame.draw.rect(pgDisplay, green, (150, 450, 100, 50))
 
-        pgDisplay.blit(title, (350,10))
+        mouse = pygame.mouse.get_pos()
+
+        if  event.type == MOUSEBUTTONUP and 150+100 > mouse[0] > 150 and 450+50 > mouse[1] > 450:
+            pygame.draw.rect(pgDisplay, green, (150, 450, 100, 50))
+            print('Hello')
+            pgDisplay1.fill((125, 125, 125))
 
         pygame.display.update()
-        clock.tick(15)
+        #clock.tick(15)
+
 
 
 game_intro()
